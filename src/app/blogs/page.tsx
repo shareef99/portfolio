a203cc7dc99ecbfef67ba03e633772a353cb3ba9
NadeemShareef <105916625+NadeemShareef@users.noreply.github.com>
2023-11-26 19:02:17 +0530

    Design blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,57 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { blogList } from "@/types/blog";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Blogs | Shareef",
+  description:
+    "I write blogs about the problems I encounter during development of softwares and how I solve them.",
+};
+
+export default async function Page() {
+  const getBlogs = async () => {
+    const res = await fetch("https://dev.to/api/articles?username=shareef");
+    const data = await res.json();
+    return data as blogList[];
+  };
+
+  const blogs = await getBlogs();
+
+  return (
+    <main className="container">
+      <section className="my-8 lg:my-16">
+        <h1 className="text-3xl mb-8 underline underline-offset-8">
+          All Blogs
+        </h1>
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          {blogs.map((blog) => (
+            <Card key={blog.id}>
+              <CardHeader className="space-y-2">
+                <CardTitle>
+                  <Link href={blog.url}>{blog.title}</Link>
+                </CardTitle>
+                <CardDescription>{blog.description}</CardDescription>
+              </CardHeader>
+              <CardFooter className="flex gap-2 flex-wrap lg:gap-4">
+                <Badge variant="secondary">
+                  {blog.reading_time_minutes} mins
+                </Badge>
+                {blog.tag_list.map((tag) => (
+                  <Badge key={tag}>{tag}</Badge>
+                ))}
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
