6871e269e1d6dacdc7fa626c39d5549abe40c1e5
NadeemShareef <nadeemshareef934@gmail.com>
2023-11-27 07:17:19 +0530

    update meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,8 @@ const noto = Noto_Sans({
 
 export const metadata: Metadata = {
   title: "Nadeem Shareef",
-  description: "Portfolio Website of Nadeem Shareef",
+  description:
+    "I'm Nadeem Shareef a passionate software developer who loves building awesome things on the internet, especially the parts you can see and play with. Over the past two years, I've been getting better at building frontend of websites, with a focus on making websites not only functional but also look and feel fantastic. The tools I use are ReactJS, NextJS, TypeScript, JavaScript, TailwindCSS, TanStack Query(React Query), Redux, HTML, CSS, SCSS, and many other react libraries to bring digital ideas to life.",
 };
 
 export default function RootLayout({
