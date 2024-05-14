f6819696fda6ba3a0e02b5e3d6b4dfb6d88b6447
Nadeem Shareef <nadeemshareef934@gmail.com>
2024-05-14 23:15:00 +0530

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <main className="container my-8 lg:my-12">
+      <h1 className="text-3xl mb-8">About Me</h1>
+      <p>
+        I am not a <span className="italic">“React Developer”</span>,{" "}
+        <span className="italic">“Frontend Developer”</span>, or{" "}
+        <span className="italic">“Framework Developer”</span>.I am a software
+        developer. I solve problems. I love solving problems related to the user
+        experience of the software.
+      </p>
+    </main>
+  );
+}
